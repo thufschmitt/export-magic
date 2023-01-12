6955e8ff1b47e94f846c50068af454b320ff0b0b
Max <max@privatevoid.net>
2023-01-12 22:35:05 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }: let
+    version = builtins.match ".*tag: ([0-9\.]*).*" (builtins.readFile ./.version);
+  in {
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.writeText "text-${version}" ''
+      This is version ${version}
+    '';
+  };
+}
