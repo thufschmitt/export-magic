9bebe1603582c8cc5d6d971e31914a35bb85bbf1
Max <max@privatevoid.net>
2023-01-12 22:36:34 +0100

    forgor

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs, ... }: let
-    version = builtins.match ".*tag: ([0-9\.]*).*" (builtins.readFile ./.version);
+    version = builtins.head (builtins.match ".*tag: ([0-9\.]*).*" (builtins.readFile ./.version));
   in {
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.writeText "text-${version}" ''
       This is version ${version}
