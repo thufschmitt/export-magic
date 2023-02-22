9d5161b9d1da83e105e4a86696f6977d673cb169
Tom Bereknyei <tomberek@gmail.com>
2023-02-21 19:50:05 -0500

    asdf

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs, ... }: let
-    version = builtins.head (builtins.match ".*tag: ([0-9\.]*).*" (builtins.readFile ./.version));
+    version = builtins.readFile ./.version;
   in {
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.writeText "text-${version}" ''
-      This is version ${version}
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.runCommand "text" {} ''
+      echo "${version}"
     '';
   };
 }
